bffb7af46af02574cf40a608be30e346c14f3dbd
Muhammad Alfin Zidane Rahman <alfin.zidane@gmail.com>
2023-05-08 15:07:42 +0700

    adding optional FindByUserOrEmail

--- src/main/java/com/mccserverapp/project/Repository/UserRepository.java
+++ src/main/java/com/mccserverapp/project/Repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.mccserverapp.project.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.mccserverapp.project.Model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsernameOrEmployee_Email(String username, String email);
 }
