92e9ef3420787c0a2d34e527a89aa3abbe4bae28
Muhammad Alfin Zidane Rahman <alfin.zidane@gmail.com>
2023-05-11 21:19:49 +0700

    configuring authentication service for login and register

--- src/main/java/com/mccserverapp/project/Service/AuthService.java
+++ src/main/java/com/mccserverapp/project/Service/AuthService.java
@@ -18,6 +18,7 @@ import com.mccserverapp.project.Model.Employee;
 import com.mccserverapp.project.Model.Role;
 import com.mccserverapp.project.Model.User;
 import com.mccserverapp.project.Model.dto.request.LoginRequest;
+import com.mccserverapp.project.Model.dto.request.RegisterRequest;
 import com.mccserverapp.project.Model.dto.request.UserRequest;
 import com.mccserverapp.project.Model.dto.response.LoginResponse;
 import com.mccserverapp.project.Repository.UserRepository;
@@ -28,57 +29,76 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class AuthService {
 
-    private AuthenticationManager authenticationManager;
-    private UserRepository userRepository;
-    private AppUserDetailService appUserDetailService;
-    private ModelMapper modelMapper;
-    private PasswordEncoder passwordEncoder;
-    private RoleService roleService;
+        private AuthenticationManager authenticationManager;
+        private UserRepository userRepository;
+        private AppUserDetailService appUserDetailService;
+        private ModelMapper modelMapper;
+        private PasswordEncoder passwordEncoder;
+        private RoleService roleService;
 
-    public LoginResponse login(LoginRequest loginRequest) {
-        // login request
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(
-                loginRequest.getUsername(),
-                loginRequest.getPassword());
+        public LoginResponse login(LoginRequest loginRequest) {
+                // login request
+                UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(
+                                loginRequest.getUsername(),
+                                loginRequest.getPassword());
 
-        // set principle
-        Authentication auth = authenticationManager.authenticate(authReq);
-        SecurityContextHolder.getContext().setAuthentication(auth);
+                // set principle
+                Authentication auth = authenticationManager.authenticate(authReq);
+                SecurityContextHolder.getContext().setAuthentication(auth);
 
-        User user = userRepository
-                .findByUsernameOrEmployee_Email(loginRequest.getUsername(), loginRequest.getUsername()).get();
+                User user = userRepository
+                                .findByUsernameOrEmployee_Email(loginRequest.getUsername(), loginRequest.getUsername())
+                                .get();
 
-        UserDetails userDetails = appUserDetailService.loadUserByUsername(
-                loginRequest.getUsername());
+                UserDetails userDetails = appUserDetailService.loadUserByUsername(
+                                loginRequest.getUsername());
 
-        List<String> authorities = userDetails
-                .getAuthorities()
-                .stream()
-                .map(authority -> authority.getAuthority())
-                .collect(Collectors.toList());
+                List<String> authorities = userDetails
+                                .getAuthorities()
+                                .stream()
+                                .map(authority -> authority.getAuthority())
+                                .collect(Collectors.toList());
 
-        return new LoginResponse(
-                user.getUsername(),
-                user.getEmployee().getEmail(),
-                authorities);
+                return new LoginResponse(
+                                user.getUsername(),
+                                user.getPassword(),
+                                authorities);
 
-    }
+        }
 
-    public User create(UserRequest userRequest) {
-        Employee employee = modelMapper.map(userRequest, Employee.class);
-        User user = modelMapper.map(userRequest, User.class);
+        // public User create(UserRequest userRequest) {
+        //         Employee employee = modelMapper.map(userRequest, Employee.class);
+        //         User user = modelMapper.map(userRequest, User.class);
 
-        // set password
-        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        //         // set password
+        //         user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
 
-        // set role
-        List<Role> role = new ArrayList<>();
-        role.add(roleService.getById(1));
-        user.setRole(role);
+        //         // set role
+        //         List<Role> role = new ArrayList<>();
+        //         role.add(roleService.getById(1));
+        //         user.setRole(role);
 
-        employee.setUser(user);
-        user.setEmployee(employee);
-        return userRepository.save(user);
-    }
+        //         employee.setUser(user);
+        //         user.setEmployee(employee);
+        //         return userRepository.save(user);
+        // }
+
+        public User create(RegisterRequest registerRequest) {
+                //harus ditambahin try catch
+                Employee employee = modelMapper.map(registerRequest, Employee.class);
+                User user = modelMapper.map(registerRequest, User.class);
+
+                // set password
+                user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+
+                // set role
+                List<Role> role = new ArrayList<>();
+                role.add(roleService.getById(1));
+                user.setRole(role);
+
+                employee.setUser(user);
+                user.setEmployee(employee);
+                return userRepository.save(user);
+        }
 
 }
