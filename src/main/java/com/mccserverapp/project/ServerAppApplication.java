0efbacfc8911148bded2f508957af74f9f988bb4
Muhammad Alfin Zidane Rahman <alfin.zidane@gmail.com>
2023-05-15 15:57:47 +0700

    adding configuration

--- src/main/java/com/mccserverapp/project/ServerAppApplication.java
+++ src/main/java/com/mccserverapp/project/ServerAppApplication.java
@@ -2,8 +2,12 @@ package com.mccserverapp.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.mccserverapp.project.Util.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class ServerAppApplication {
 
 	public static void main(String[] args) {
