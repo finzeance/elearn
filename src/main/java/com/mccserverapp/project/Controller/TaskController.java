afb938552dfbd012906af112c3d4a1af356f2515
Muhammad Alfin Zidane Rahman <alfin.zidane@gmail.com>
2023-05-15 15:55:39 +0700

    adding endpoint for MultipartFile attach

--- src/main/java/com/mccserverapp/project/Controller/TaskController.java
+++ src/main/java/com/mccserverapp/project/Controller/TaskController.java
@@ -1,7 +1,16 @@
 package com.mccserverapp.project.Controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +19,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.mccserverapp.project.Model.Task;
 import com.mccserverapp.project.Model.dto.request.TaskRequest;
+import com.mccserverapp.project.Service.FileStorageService;
 import com.mccserverapp.project.Service.TaskService;
 
 import lombok.AllArgsConstructor;
@@ -25,6 +37,8 @@ import lombok.AllArgsConstructor;
 public class TaskController {
 
     private TaskService taskService;
+    private FileStorageService fileStorageService;
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     @PreAuthorize("hasAuthority('READ_STUDENT', 'READ_MANAGER', 'READ_TRAINER')")
     @GetMapping
@@ -52,8 +66,8 @@ public class TaskController {
 
     @PreAuthorize("hasAuthority('CREATE_MANAGER', 'CREATE_TRAINER')")
     @PostMapping("/modelmapper")
-    public Task createWithModelMapper(@RequestBody TaskRequest taskRequest) {
-        return taskService.createWithModelMapper(taskRequest);
+    public Task createWithModelMapper(TaskRequest taskRequest, @RequestParam MultipartFile file) {
+        return taskService.createWithModelMapper(taskRequest, file);
     }
 
     @PreAuthorize("hasAuthority('UPDATE_MANAGER', 'UPDATE_TRAINER')")
@@ -66,5 +80,30 @@ public class TaskController {
     @DeleteMapping("/{id}")
     public Task delete(@PathVariable Integer id) {
         return taskService.delete(id);
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        // load file as resource
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        // try to dertermine file's content type
+        String contentType = null;
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException e) {
+            logger.info("Could not determine file type.");
+        }
+
+        // fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
